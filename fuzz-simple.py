7b5c122639341cd1c3d154bc76b2b072a10342ce
root <root@localhost.localdomain>
2016-05-08 22:17:54 -0400

    add tftp post exploitation notes and update sl mail script because there was a bug in connect part of the python script

--- fuzz-simple.py
+++ fuzz-simple.py
@@ -12,7 +12,7 @@ while len(buffer) <= 30:
 for string in buffer:
 	print "Fuzzing PASS with %s bytes" % len(string)
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	connect=s.connect(('192.168.15.34'),110)
+	connect = s.connect(('192.168.15.162',110))
 	s.recv(1024)
 	s.send('USER test \r\n')
 	s.recv(1024)
